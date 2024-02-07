cbd98e0399fedaaf07962aa37224137d32457676
jspvg <josipovic1501@gmail.com>
2024-02-07 12:30:27 +0100

    add theme toggle with context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import ToggleTheme from "./ToggleTheme";
 
 const Navbar = () => {
   const { asPath } = useRouter();
   return (
-    <nav>
-      <Link href="/" aria-current={asPath === "/" ? "page" : undefined}>
-        about me
-      </Link>
-      <Link
-        href="projects"
-        aria-current={asPath === "/projects" ? "page" : undefined}
-      >
-        projects
-      </Link>
-      <Link
-        href="education"
-        aria-current={asPath === "/education" ? "page" : undefined}
-      >
-        education
-      </Link>
-    </nav>
+    <div className="navigation">
+      <nav>
+        <Link href="/" aria-current={asPath === "/" ? "page" : undefined}>
+          about me
+        </Link>
+        <Link
+          href="projects"
+          aria-current={asPath === "/projects" ? "page" : undefined}
+        >
+          projects
+        </Link>
+        <Link
+          href="education"
+          aria-current={asPath === "/education" ? "page" : undefined}
+        >
+          education
+        </Link>
+      </nav>
+      <ToggleTheme />
+    </div>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import "@/styles/globals.css";
+import "@/styles/toggle.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
