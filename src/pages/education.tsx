a97485fefb8481e71dd33d1f4cb78928dbdae157
jspvg <josipovic1501@gmail.com>
2024-02-18 21:45:09 +0100

    create scaffold for edu page

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,5 +1,51 @@
 const Education = () => {
-  return <div className="page">Education</div>;
+  return (
+    <div className="page">
+      <h5>university</h5>
+      <div className="uni">
+        <div className="uni-header">
+          <p>Bachelor of Computing [univ. bacc. ing. comp.]</p>
+        </div>
+        <div className="uni-body">
+          <p>
+            Worked on multiple projects utilising different technologies mostly
+            in Web Development <br /> Tutored and mentored students in the
+            fields of Mathematics and Programming (Data Structures, OOP, C/C++,
+            Java)
+            <br />
+            Thesis: Possibilities of web application development in the Kotlin
+            programming language
+          </p>
+        </div>
+      </div>
+      <div className="certs">
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+        <div className="card-cert">
+          <h6>Devademy: Space Edition [React]</h6>
+          <small>aug 2023, udemy</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
